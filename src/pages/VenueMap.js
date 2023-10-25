b0de803d98aa29f0684b2dcb30b7857cdcf4da08
RishilU <112522578+RishilU@users.noreply.github.com>
2023-10-24 20:57:01 -0500

    Added a Venuemap

--- src/pages/VenueMap.js
+++ src/pages/VenueMap.js
@@ -1,13 +1,23 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Text } from '@chakra-ui/react'
 import * as React from 'react'
 import { Container } from '@chakra-ui/react'
+import Header from '../components/Header'
+
 
 
 
 function VenueMap() {
     return (
     <Container>
-        "hi ur moms a bigger hoe"
+        <Text fontSize="x1" textAlign="center">Welcome to quite literally the greatest venue in the history of mankind any bad biddies hit my dms -NotRishi  </Text>
+        <iframe
+        title="Venue Map"
+        width="700"
+        height="550"
+        frameBorder="0"
+        src="https://www.uta.edu/maps/embed?id=54"
+        allowFullScreen
+    ></iframe>
     </Container>
     )
   }
