ebe99eaae035cf63b6dc6bf4a4ba24778c59c07f
RishilU <112522578+RishilU@users.noreply.github.com>
2023-11-14 04:04:50 -0600

    Map Changes

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,16 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider,Container,Image } from '@chakra-ui/react'
 import * as React from 'react'
-import { Container } from '@chakra-ui/react'
-
 
 
 function AboutUs() {
+ 
     return (
     <Container>
         "DTX Dandiya is a Raas/Garba Competition hosted by the students of the University of Texas at Dallas.Founded in 2019, 
         we aim to bring awareness of Garba/Raas to the Dallas area. Our competition give teams around the US an opportunity
          to express themselves through this form of dance. Our goal is to bring our community together through dance 
-         and raising awareness through dance. "
+         and raising awareness through dance. " 
     </Container>
     )
   }
