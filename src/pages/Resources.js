d1846e717f3b0a9ce62ecd7f1afc360e3a0ce11c
RishilU <112522578+RishilU@users.noreply.github.com>
2024-01-16 14:56:24 -0600

    Hi this is merge

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,15 +1,51 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import * as React from 'react'
-import { Container } from '@chakra-ui/react'
 
+import React from "react";
+import {
+  Flex,
+  Table,
+  TableCaption,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Button,
+  VStack,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
+const Resources = () => {
+  // Generate fake data for the table
+  const fakeData = [
+    { name: "John Doe", phoneNumber: "555-1234" },
+    // Add more fake data as needed
+  ];
 
-function Team1() {
-    return (
-    <Container>
-        "hi"
-    </Container>
-    )
-  }
-  
-  export default Team1;
+  return (
+    <Flex bg="palette.bgDarkGreen" color="palette.dtxGold" w="100%" justify="center">
+      <VStack>
+        <Table variant="simple" size={{ base: "sm", md: "lg", lg: "lg" }} colorScheme="palette.dtxGold">
+          <TableCaption placement="top" color="palette.dtxGold">
+            {/* Add any caption content if needed */}
+          </TableCaption>
+          <Thead color="palette.dtxGold" bg="palette.dtxGold">
+            <Tr>
+              <Th color="palette.bgDarkGreen">Name</Th>
+              <Th color="palette.bgDarkGreen">Phone Number</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {fakeData.map((item, index) => (
+              <Tr key={index}>
+                <Td>{item.name}</Td>
+                <Td>{item.phoneNumber}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default Resources;
